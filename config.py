b38e0d4ed96fca505bf93c657bc216a957ff22e7
KSUN-developer <sunkang_99@126.com>
2025-07-29 01:56:42 +0200

    db from mySQL to postgres

--- config.py
+++ config.py
@@ -6,8 +6,9 @@ class Config:
     LANGUAGES = ['zh', 'nl']
     BASEDIR = os.path.abspath(os.path.dirname(__file__))
 
-    SQLALCHEMY_DATABASE_URI = os.environ.get("mysql+pymysql://kang:Welkom123%@localhost/yischool")
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    #SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL", "mysql+pymysql://kang:Welkom123@localhost/yischool")
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+	SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     # config.py
     UPLOAD_FOLDER = os.path.join(os.path.abspath(os.path.dirname(__file__)),'app', 'static', 'resources')
